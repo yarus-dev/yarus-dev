91fcd1471f1795a0cca55c857f9daf8c52578c97
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-02-08 10:58:38 +0200

    create simple site

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://yarus.app',
+      lastModified: new Date(),
+      changeFrequency: 'Monthly',
+      priority: 1,
+    }
+  ]
+}
