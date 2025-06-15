580f879f8d627bf65c4a543118ae9fe970cb7be0
yarus-dev <152315680+yarus-dev@users.noreply.github.com>
2025-06-15 08:49:27 +0300

    fix fucking deploy \

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'build',
-  trailingSlash: true,
   experimental: {
     mdxRs: true,
   },
